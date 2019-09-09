05e4a9a69477c390e04d85d769e2e94251e82137
gediont <teshomegedion@gmail.com>
2019-09-09 09:53:01 +0300

    Formatted

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-module.exports = function Capser(string){
+module.exports = function Capser(string) {
 
 	var stash = [];
 
-	if(string != " ") {
+	if (string != " ") {
 
- 			string = string.split('');
+		string = string.split('');
 
-         for(var i=0; i < string.length ; i++)
-         { 
- 		         stash.push(string[i]);
-         }
+		for (var i = 0; i < string.length; i++) {
+			stash.push(string[i]);
+		}
 
-         for(var i=0; i < string.length; i++)
-         {
-         		if(i==0)
-         		{
-         			var letter = stash[0];
-         			stash[0]   = letter.toUpperCase();
-         		}
+		for (var i = 0; i < string.length; i++) {
+			if (i == 0) {
+				var letter = stash[0];
+				stash[0] = letter.toUpperCase();
+			}
 
-         		if(stash[i] == " ")
-         		{
-         			var letter = stash[i+1];
-         			stash[i+1] = letter.toUpperCase();
-         		}
+			if (stash[i] == " ") {
+				var letter = stash[i + 1];
+				stash[i + 1] = letter.toUpperCase();
+			}
 
 
-         }
- 	}
- 		 
- 		return stash.join('');
+		}
+	}
 
+	return stash.join('');
 }
